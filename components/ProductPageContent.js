29361b2610dba1574f8100449950bdeeda73b809
Jolanda Keizer <keizer2012@gmail.com>
2023-05-22 08:02:46 +0200

    product form

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,9 +1,21 @@
+import Image from 'next/image'
+import ProductForm from './ProductForm'
 
 export default function ProductPageContent({product}) {
   return (
-    <div>
-            {product.title}
-
+    <div className="flex flex-col items-center justify-center w-11/12 max-w-6xl mx-auto space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8">
+      <div className="w-full max-w-md overflow-hidden bg-white border shadow-lg rounded-2xl md:w-1/2">
+        <div className="relative w-full h-auto">
+          <Image 
+          src={product.images.edges[0].node.url} 
+          alt={product.images.edges[0].node.altText} 
+          style={{objectFit:"fill"}} 
+          width="500" 
+          height="1000"
+          />
+        </div>
+      </div>
+      <ProductForm product={product}/>
     </div>
   )
 
